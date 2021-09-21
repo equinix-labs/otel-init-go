31e58cd326f6cbc00e9d6c0851ee8518d24e7214
Amy Tobey <atobey@equinix.com>
2021-09-21 16:24:49 -0700

    Add helpers (#3)
    * go mod tidy
    * add simple trace propagation carrier and helper for envvars
    
    No tests for now, we'll add those in a follow-up PR if this works out.
    
    Signed-off-by: Amy Tobey <atobey@equinix.com>

---
+++ otelhelpers/simple_carrier.go
@@ -0,0 +1,33 @@
+package otelhelpers
+
+// SimpleCarrier is an abstraction for handling traceparent propagation
+// that needs a type that implements the propagation.TextMapCarrier().
+// This is the simplest possible implementation that is a little fragile
+// but since we're not doing anything else with it, it's fine for this.
+type SimpleCarrier map[string]string
+
+// Get implements the otel interface for propagation.
+func (otp SimpleCarrier) Get(key string) string {
+	return otp[key]
+}
+
+// Set implements the otel interface for propagation.
+func (otp SimpleCarrier) Set(key, value string) {
+	otp[key] = value
+}
+
+// Keys implements the otel interface for propagation.
+func (otp SimpleCarrier) Keys() []string {
+	out := []string{}
+	for k := range otp {
+		out = append(out, k)
+	}
+	return out
+}
+
+// Clear implements the otel interface for propagation.
+func (otp SimpleCarrier) Clear() {
+	for k := range otp {
+		delete(otp, k)
+	}
+}
